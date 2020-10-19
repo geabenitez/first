04b07654a0081fc5f7c7d4868ec09392f2c13c96
Gerson E. Aguirre <geabenitez@laptop.localdomain>
2020-10-19 00:01:26 -0600

    Clase Uno - COnfiguracion vue - Configuracion tailwind

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Bai</p>
+    <p>Bai</p>
+    <p>Bai</p>
+    <p>Bai</p>
+    <p>Bai</p>
+    <p>Bai</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
---
+++ src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-row border-b justify-between py-4">
+    <span>VUEJS</span>
+    <ul class="flex flex-row space-x-6">
+      <li v-for="(m, k) of menu" :key="k" class="cursor-pointer hover:font-black">{{ m }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return {
+      menu: [
+        'Inicio',
+        'Productos',
+        'Sobre nosotros',
+        'Contacto',
+        'Otro',
+      ]
+    }
+  }
+}
+</script>
